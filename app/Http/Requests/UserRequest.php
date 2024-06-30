4808ce4532fdfe9e30603653c79f6be6fb264ad4
Kolawole <kolakachi@gmail.com>
2024-06-30 11:28:31 +0100

    Add custom UserRequest for user-related API validation
    Created UserRequest class to handle validation logic for user-related API endpoints.

---
+++ app/Http/Requests/UserRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UserRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules(): array
+    {
+        return [
+            'first_name' => 'required|string|max:255',
+            'last_name' => 'required|string|max:255',
+            'email' => 'required|string|email|max:255|unique:users',
+            'password' => 'required|string|min:8',
+            'password_confirmation' => 'required|same:password',
+            'address' => 'required|string|max:255',
+            'phone_number' => 'required|string|max:20',
+        ];
+    }
+}
