04734f9460fa992a3611307a35b7223d4bdd1c7e
Kolawole <kolakachi@gmail.com>
2024-07-02 15:26:00 +0100

    Create ProductController

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * @OA\Tag(
+ *     name="Products",
+ *     description="API Endpoints for Products"
+ * )
+ */
+class ProductController extends Controller
+{
+    //
+}
