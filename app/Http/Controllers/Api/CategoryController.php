18a18c0d4f0269cb641f1cd98487e8db83cb2e81
Kolawole <kolakachi@gmail.com>
2024-07-02 14:44:53 +0100

    Create CategoryController

---
+++ app/Http/Controllers/Api/CategoryController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+/**
+ * @OA\Tag(
+ *     name="Categories",
+ *     description="API Endpoints for Categories"
+ * )
+ */
+class CategoryController extends Controller
+{
+    //
+}
