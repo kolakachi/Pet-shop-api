528e3dcc7a517ed657af25b4f10a57721e179b95
Kolawole <kolakachi@gmail.com>
2024-07-02 11:46:32 +0100

    Create FileController

---
+++ app/Http/Controllers/Api/FileController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+
+/**
+ * @OA\Tag(
+ *     name="File",
+ *     description="File API endpoint"
+ * )
+ */
+class FileController extends Controller
+{
+    //
+}
